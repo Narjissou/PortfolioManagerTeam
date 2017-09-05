03731bd6ec564a1a448290dfdb47332e87b6154d
yapexx <yannx.b@hotmail.fr>
2017-09-05 15:10:09 +0200

    ajout option

--- PortfolioManagerTeam/ProjetPortfolioNet/ProjetPortfolioNet/Modele/Option.cs
+++ PortfolioManagerTeam/ProjetPortfolioNet/ProjetPortfolioNet/Modele/Option.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace ProjetPortfolioNet.Modele
 {
-    class Class2
+    class Option
     {
+        #region Private Fields
+
+        private string name;
+        private double strike;
+        private double price;
+        private double vol;
+        private int maturity;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public Option(string name, double strike, double price, double vol, int maturity)
+        {
+            this.name = name;
+            this.strike = strike;
+            this.price = price;
+            this.vol = vol;
+            this.maturity = maturity;
+        }
+
+        #endregion Public Constructors
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public double getStrike()
+        {
+            return this.strike;
+        }
+
+        public double getVol()
+        {
+            return this.vol;
+        }
+
+        public double getMaturity()
+        {
+            return this.maturity;
+        }
     }
 }
