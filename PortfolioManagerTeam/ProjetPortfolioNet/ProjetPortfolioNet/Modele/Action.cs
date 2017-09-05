2f631c1f9eb8bcdbc73fb5718747bc032fa067de
bourin <narjisse.bouri@grenoble-inp.org>
2017-09-05 15:03:27 +0200

    classe action

---
+++ PortfolioManagerTeam/ProjetPortfolioNet/ProjetPortfolioNet/Modele/Action.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetPortfolioNet.Modele
+{
+    class Action
+    {
+        #region Private Fields
+
+        private string name; 
+        private double price;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public Action(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        #endregion Public Constructors
+
+
+        #region Public Methods
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public double getPrice()
+        {
+            return this.price;
+        }
+
+        public void setPrice(double p)
+        {
+            price = p;
+        }
+
+        #endregion Public Methods
+
+    }
+}
